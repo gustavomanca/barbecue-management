3ecb397b6e25bcf39b17381ee02ad7dca83cd49e
Gustavo Manca <dev3@easycrm.com.br>
2022-07-15 13:38:05 -0300

    feat(TextField): add forwardRef to handle with focus

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,14 +1,14 @@
-import { InputHTMLAttributes } from 'react'
+import { forwardRef, InputHTMLAttributes } from 'react'
 import * as S from './styles'
 
 type Props = InputHTMLAttributes<HTMLInputElement>
 
-function TextField(props: Props) {
-  return (
-    <S.Container>
-      <S.Input {...props} />
-    </S.Container>
-  )
-}
+const TextField = forwardRef<HTMLInputElement, Props>((props, ref) => (
+  <S.Container>
+    <S.Input ref={ref} {...props} />
+  </S.Container>
+))
+
+TextField.displayName = 'TextField'
 
 export default TextField
